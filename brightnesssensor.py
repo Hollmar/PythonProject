f1a52fd86793ba969cb165d851085f7ebc6d76c7
Filip Brkic <filipb@gmx.net>
2020-11-22 18:40:59 +0100

    Deleting brightnesssensor.py

--- brightnesssensor.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
-from device import Device
-
-class BrightnessSensor(Screen):
-    def create_screen(self,device):
-        self.ids.label1.text = device.Name
-        self.ids.label3.text = "EUI64: "+device.EUI64
-
